ac9b40d73a98ac7733dc687e43262f3901840b04
wangzhenpeng <1228916292@qq.com>
2018-12-03 00:17:36 +0800

    王振鹏-添加Header

---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-header class="header">
+      <el-row>
+        <el-col>
+          <div class="head-wrap">Element</div>
+        </el-col>
+        <el-col>
+          <el-tabs>test</el-tabs>
+        </el-col>
+      </el-row>
+    </el-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      placeHolder: 'this is header'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .header{
+    /*background-color: #409EFF;*/
+    background-color: #52BE80;
+    color: #fff;
+    line-height: 60px;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="Home">
+    <el-container>
+        <commonHeader/>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import CommonHeader from '@/components/CommonHeader'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      placeHolder: 'this is Home'
+    }
+  },
+  components: {
+    'commonHeader': CommonHeader
+  }
+}
+</script>
+
+<style scoped>
+</style>
